5b540019baaf70f2750ae04fff158674f1761deb
KeelyBrown <156026125+k034b363@users.noreply.github.com>
2026-08-14 13:53:35 -0500

    update name of test notebook in test file

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,7 @@ def test_notebook(tmpdir):
     """Test the notebook."""
     tmp = tmpdir.mkdir('sub')
     # Open the notebook
-    with open("Test_index.ipynb", "r") as f:
+    with open("test_index.ipynb", "r") as f:
         nb = nbformat.read(f, as_version=4)
 
     # Process the notebook
